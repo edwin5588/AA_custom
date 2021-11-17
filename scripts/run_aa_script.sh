b0a0adf80e7a5ea4d4470a24af72fb6619307a57
ted Liefeld <liefeld@broadinstitute.org>
2021-11-17 13:52:04 -0800

    Finally a working version - except for downsample

--- scripts/run_aa_script.sh
+++ scripts/run_aa_script.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 echo "Running in $PWD">pwd.txt
-mkdir $PWD/genome/$1
+mkdir -p $PWD/genome/$1
 
-tar -xf /opt/genepatt/data_repo/$1.tar.gz --directory ./genome/$1
+tar -xf /opt/genepatt/data_repo/$1.tar.gz --directory ${PWD}/genome/$1
 
 # cp $2 /opt/genepatt/programs/mosek/8/licenses/
 # cp $2 /opt/genepatt/programs/mosek/
 
 MOSEKLM_LICENSE_FILE=$2
 export MOSEKLM_LICENSE_FILE
-AA_DATA_REPO=/opt/genepatt/data_repo
+AA_DATA_REPO=${PWD}/genome/
 export AA_DATA_REPO
 DOWNSAMPLE = $7
 export DOWNSAMPLE
@@ -30,7 +30,7 @@ bed="$bedpath/$bedbase"
 bai="$baipath/$baibase"
 
 
-python2 /opt/genepatt/AA/AmpliconArchitect.py --bam $bam --bed $bed --out $5 --ref ./genome/${1}
+python2 /opt/genepatt/AA/AmpliconArchitect.py --bam $bam --bed $bed --out $5 --ref $PWD/genome/${1}
 
 # remove the genome so it does not show as an output
 rm -rf ./genome
